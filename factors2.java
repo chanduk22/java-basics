2ae83aafaad9768c0897c168cdee5f7bccfe2583
K.CHANDU KUMAR <126059033+chanduk22@users.noreply.github.com>
2023-03-13 19:35:09 +0530

    factors2.java

---
+++ factors2.java
@@ -0,0 +1,29 @@
+/*Factors of a given number L1
+Write a program that can compute the factors of a given number. The results should be printed in a comma-separated sequence on a single line.
+
+Input :
+
+10
+
+output :
+
+1 2 5 */
+
+
+import java.util.Scanner;
+public class factors
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int i;
+        for(i=1;i<n;i++)
+        {
+            if(n%i==0)
+            {
+                System.out.printf("% d",i);
+            }
+        }
+    }
+}
