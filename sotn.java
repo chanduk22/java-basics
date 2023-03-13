f671cc43c2b69feb3f58291ce65b199373717d42
K.CHANDU KUMAR <126059033+chanduk22@users.noreply.github.com>
2023-03-13 19:37:09 +0530

    sotn.java

---
+++ sotn.java
@@ -0,0 +1,31 @@
+/*sum of two numbers-L1
+Define a function which can compute the sum of two numbers using recursion.
+
+sample input:
+
+4
+
+8
+
+Sample output:
+
+12 */
+
+
+import java.util.Scanner;
+public class sotn{
+    public static void main(String[] args){
+        
+      sum.add();
+    }
+}
+class sum{
+    public static void add(){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=a+b;
+        System.out.println(c);
+    }
+}
+
