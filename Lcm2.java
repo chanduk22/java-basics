5725120a6b0194f223b0767871347c430476899f
K.CHANDU KUMAR <126059033+chanduk22@users.noreply.github.com>
2023-03-13 18:49:07 +0530

    Lcm2.java

---
+++ Lcm2.java
@@ -0,0 +1,46 @@
+/*Method-LCM of two numbers
+Write a program to find LCM of two numbers is discussed here. Two numbers are obtained as input and the prime factors of both the numbers are found. The product of the union of prime factors of both the numbers gives the LCM of the two numbers.
+
+Input & Output Format:
+
+Input consists of two integers.
+
+Output consists of the lcm of two numbers.
+
+Sample Input:
+
+5
+
+30
+
+Sample Output:
+
+LCM of 5 and 30 is 30 */
+
+
+import java.util.Scanner;
+public class Lcm2
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);  
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int i=demo.lcm(x,y);
+        System.out.printf("LCM of %d and %d is %d",x,y,i);
+    }
+}
+class demo
+{
+    public static int lcm(int x,int y)
+    {
+        int a=(x>y)?x:y;
+        int i;
+        for( i=a;i<=x*y;i=i+a)
+        {
+            if(i%x==0 && i%y==0)
+            break;
+        }
+        return i;
+    }
+}
